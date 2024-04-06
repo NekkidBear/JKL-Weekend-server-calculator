406b0b7965cb221c7425f953fddc24eec43489ad
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-06 11:31:00 -0500

    write calcClear, POST functions update render function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,58 +1,89 @@
-console.log('client.js is sourced!');
+console.log("client.js is sourced!");
 
-let operator = '';
+let operator = "";
+let numOne = "";
+let numTwo = "";
+
+/** this function triggers on intial page load to get the initial data fromt the server */
+function onReady() {
+  fetchCalcs();
+  renderDOM(calculations);
+}
 
 /** this function will render the updated data in the DOM */
-function renderDOM(calculations){
-    //display the result of the current function
-    let recentResultLocation = document.getElementById("recentResult")
-    recentResultLocation.textContent = calculations.at(-1).result;
-    //display the history of the calculations
-    let calcHistoryULlocation = document.getElementById("calcHistoryUL");
-    for (let calc of calculations){
-        calcHistoryULlocation += `
-        <li>${calc}</li>`;
-    }
-    //clear the inputs for another calculation
+function renderDOM(calculations) {
+  //display the result of the current function
+  let recentResultLocation = document.getElementById("recentResult");
+  recentResultLocation.textContent = calculations.at(-1).result;
+  //display the history of the calculations
+  let calcHistoryULlocation = document.getElementById("calcHistoryUL");
+  for (let calc of calculations) {
+    calcHistoryULlocation += `
+        <li>${calc.numOne} ${calc.operator} ${calc.numTwo} = ${calc.result}</li>`;
+  }
+  //clear the inputs for another calculation
+  calcClear();
 }
 //****************************** *//
 //Operation buttons
 //****************************** */
 
 /** this function sets the calculation operator to '+' */
-function calcAdd(event){
-    operator= '+';
+function calcAdd(event) {
+  operator = "+";
 }
 
 /** this function sets the calculation operator to '-' */
-function calcSubtract(event){
-    operator= '-';
+function calcSubtract(event) {
+  operator = "-";
 }
 
 /** this function sets the calculation operator to '*' */
-function calcMultiply(event){
-    operator= '*';
+function calcMultiply(event) {
+  operator = "*";
 }
 
 /** this function sets the calculation operator to '*' */
-function calcDivide(event){
-    operator= '/';
+function calcDivide(event) {
+  operator = "/";
 }
 
 //****************************** */
 //execution buttons
 //****************************** */
 /** this function generates the POST request to trigger the calculations on the server  */
-function calcEquals(event){
-    //todo POST request
+function calcEquals(event) {
+  //create data object
+  numOne = document.getElementById("numOne").value;
+  numTwo = document.getElementById("numTwo").value;
+  //operator is set by the calc buttons
+
+  // send post request with calculation to perform
+  axios({
+    method: "POST",
+    url: "/calculations",
+    data: { numOne: numOne, numTwo: numTwo, operator: operator },
+  });
 }
 
-/** this function sets the calculation operator to '+' */
-function calcClear(event){
-    //todo
+/** this function clears the inputs '+' */
+function calcClear(event) {
+    numOne = document.getElementById("numOne").value;
+    numTwo = document.getElementById("numTwo").value;
+
+    numOne = '';
+    numTwo = '';
+    operator = '';
 }
 
 /** this function will fetch the new data */
-function fetchCalcs(){
-    //todo GET request
+function fetchCalcs() {
+  // GET request
+  axios({
+    method: "GET",
+    url: "/calculations",
+  }).then((res) => {
+    let calculations = res.calculations;
+    return calculations;
+  });
 }
