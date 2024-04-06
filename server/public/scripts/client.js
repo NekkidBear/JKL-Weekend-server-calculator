599696e9ac3ddd7a13322f9cb5f9df5d90356f4a
Jason King-Lowe <NekkidBearGaming@gmail.com>
2024-04-06 10:21:50 -0500

    initial commit; update HTML and client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,24 @@
 console.log('client.js is sourced!');
+
+let operator = '';
+//****************************** *//
+//Operation buttons
+//****************************** */
+
+/** this function sets the calculation operator to '+' */
+function calcAdd(event){}
+/** this function sets the calculation operator to '-' */
+function calcSubtract(event){}
+/** this function sets the calculation operator to '*' */
+function calcMultiply(event){}
+/** this function sets the calculation operator to '*' */
+function calcDivide(event){}
+
+//****************************** */
+//execution buttons
+//****************************** */
+/** this function generates the POST request to trigger the calculations on the server  */
+function calcEquals(event){}
+
+/** this function sets the calculation operator to '+' */
+function calcClear(event){}
